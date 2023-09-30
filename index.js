50f0d366c10e516077600fcf6afd0c6a64777fb9
asommacal <sommacalalberto@gmail.com>
2023-09-30 18:58:40 +0200

    add event listener for push item to chart

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 import { menuArray as Items } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+const modalForm = document.getElementById('modal')
+let chartArray = []
 
 document.addEventListener('click', function(e){
     console.log(e)
+    if(e.target.dataset.add){
+        console.log(`add button ${e.target.dataset.add}`) 
+        handleaddItemClick(e.target.dataset.add)
+    }
+    else if(e.target.id === 'order-complete-btn'){
+        console.log("confirm order button pressed") 
+        modalForm.style.display = 'inline'
+     }
+     else if(e.target.id === 'modal-close-btn'){
+        console.log("close modal btn pressed") 
+        modalForm.style.display = 'none'
+     }
+     else if(e.target.id === 'pay-btn'){
+        console.log("pay btn pressed") 
+        modalForm.style.display = 'none'
+     }
+    
 })
+
+function handleaddItemClick(itemId){
+    console.log(itemId)
+    const targerItemObj = Items.filter(function(item){
+        return item.id == itemId
+    })[0]
+
+    chartArray.push(
+        {
+            item : targerItemObj,
+            uuid: uuidv4()
+        }
+    )
+    console.log(chartArray)
+}
 
 
 
@@ -22,7 +58,7 @@ function getItemsHtml(){
                     <div class="item-add">
                         <span class="item-detail">
                             <p class="item-add-btn"
-                            data-reply="${item.id}"
+                            data-add="${item.id}"
                             >+</p>
                         </span> 
                     </div>            
